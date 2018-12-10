08e69d330e292376f93c1a0bbca9d07f81240228
derrick-long <long.derrick8@gmail.com>
2018-12-09 21:20:43 -0500

    update fetching a single contact

--- src/actions/contactActions.js
+++ src/actions/contactActions.js
@@ -1,4 +1,4 @@
-import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from './types';
+import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT, GET_CONTACT, UPDATE_CONTACT } from './types';
 import axios from 'axios';
 
 export const getContacts = () =>  async dispatch => {
@@ -6,6 +6,15 @@ export const getContacts = () =>  async dispatch => {
     ('https://jsonplaceholder.typicode.com/users');
     dispatch({
         type: GET_CONTACTS,
+        payload: res.data
+    });
+};
+
+export const getContact = (id) =>  async dispatch => {
+    const res = await axios.get
+    (`https://jsonplaceholder.typicode.com/users/${id}`);
+    dispatch({
+        type: GET_CONTACT,
         payload: res.data
     });
 };
--- src/reducers/contactReducer.js
+++ src/reducers/contactReducer.js
@@ -1,26 +1,9 @@
-import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT } from '../actions/types';
+import { GET_CONTACTS, DELETE_CONTACT, ADD_CONTACT, GET_CONTACT, UPDATE_CONTACT } from '../actions/types';
 
 const initialState = {
-    contacts :[
-        {
-        id: 1,
-        name: 'John Doe',
-        email: 'john@gmail.com',
-        phone: '555-555-5555'
-        },
-        {
-        id: 2,
-        name: 'Karen Williams',
-        email: 'karen@gmail.com',
-        phone: '444-444-4444'
-        },
-        {
-        id: 3,
-        name: 'Henry Johnson',
-        email: 'henry@gmail.com',
-        phone: '333-333-333'
-        }
-     ]
+    contacts: [],
+    contact: {}
+     
 };
 
 export default function(state= initialState, action) {
@@ -30,6 +13,11 @@ export default function(state= initialState, action) {
                 ...state,
                 contacts: action.payload
             };
+        case GET_CONTACT:
+            return {
+                ...state,
+                contact: action.payload
+            }
         case DELETE_CONTACT:
             return {
                 ...state,
